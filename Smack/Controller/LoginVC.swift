f27765caeb5bf6e79c7686f8716b65ab127ca628
Denis Rakitin <svinson@Deniss-MacBook.local>
2018-02-14 21:18:14 +0300

    added LoginVC

---
+++ Smack/Controller/LoginVC.swift
@@ -0,0 +1,26 @@
+//
+//  LoginVC.swift
+//  Smack
+//
+//  Created by Denis Rakitin on 14/02/2018.
+//  Copyright © 2018 Denis Rakitin. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func closePressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    
+
+
+}
---
+++ Smack/Utilities/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  Smack
+//
+//  Created by Denis Rakitin on 14/02/2018.
+//  Copyright © 2018 Denis Rakitin. All rights reserved.
+//
+
+import Foundation
+
+// Segues
+
+let TO_LOGIN = "toLogin"
