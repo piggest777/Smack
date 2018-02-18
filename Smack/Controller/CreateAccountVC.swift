080b549c5082b4e6acbb6fbfc5c47e55e3ed64b5
Denis Rakitin <svinson@Deniss-MacBook.local>
2018-02-18 05:25:39 +0300

    JSON blow up my brain

--- Smack/Controller/CreateAccountVC.swift
+++ Smack/Controller/CreateAccountVC.swift
@@ -12,7 +12,7 @@ class CreateAccountVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
     }
     
     @IBAction func closeBtnPressed(_ sender: Any) {
--- Smack/Utilities/Constants.swift
+++ Smack/Utilities/Constants.swift
@@ -8,8 +8,20 @@
 
 import Foundation
 
+typealias CompletionHandler = (_ Success: Bool) -> ()
+
+//URL Constants
+let BASE_URL = "https://piggychat.herokuapp.com/v1/"
+let URL_REGISTER = "\(BASE_URL)account/register"
+
 // Segues
 
 let TO_LOGIN = "toLogin"
 let TO_CREATE_ACCOUNT = "toCreateAccount"
 let UNWIND = "unwindToChannel"
+
+//User Defaults
+let LOGGED_IN_KEY = "loggedIn"
+let TOKEN_KEY = "token"
+let USER_EMAIL = "userEmail"
+
