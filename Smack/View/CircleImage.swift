c587a40887e2d56db45f1a613f95cc0a0d992342
Денис Ракитин <piggest@mail.ru>
2018-03-09 01:06:21 +0300

    Great progress with change avatar color

---
+++ Smack/View/CircleImage.swift
@@ -0,0 +1,28 @@
+//
+//  CircleImage.swift
+//  Smack
+//
+//  Created by Denis Rakitin on 08/03/2018.
+//  Copyright © 2018 Denis Rakitin. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class CircleImage: UIImageView {
+
+    override func awakeFromNib() {
+        setUpView()
+    }
+    
+    func setUpView () {
+        self.layer.cornerRadius = self.frame.width / 2
+        self.clipsToBounds = true
+    }
+    
+    override func prepareForInterfaceBuilder() {
+        super.prepareForInterfaceBuilder()
+        setUpView()
+    }
+
+}
